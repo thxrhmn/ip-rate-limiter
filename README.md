9e4adf64b418b40acffc0d0dc27830e6719f809e
thxrhmn <rahmanmuhaemin@gmail.com>
2023-12-14 10:22:46 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# RUN
+```bash
+docker compose up -d --build
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+// Middleware untuk melihat IP address
+app.use((req, res, next) => {
+  const clientIP = req.ip; // Mengambil IP address user
+  console.log(`Client dengan IP address ${clientIP} mengakses API.`);
+  next();
+});
+
+const limiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, // 1 hari
+  max: 5, // Maksimal 5 request
+  message: (req, res) => {
+    res.status(429).json({
+      status: "error",
+      message:
+        "Batas permintaan harian Anda telah tercapai. Silakan coba lagi besok.",
+    });
+  },
+});
+
+// Menggunakan rate limiter middleware
+app.use(limiter);
+
+app.get("/api/data", (req, res) => {
+  const clientIP = req.ip;
+  res.json({
+    message: "Ini adalah contoh data dari API. ini IP anda: ",
+    clientIP,
+  });
+});
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
